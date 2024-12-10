6adcb9287bccf2dde7afac51f9bf04f06a7c86cf
tudao <nevarda9416@gmail.com>
2024-12-10 16:04:31 +0700

    - [VueJS + Docker]: Sample code

---
+++ vue-cli/src/components/Form.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>
+      {{ price }}
+    </h1>
+    <button @click="clicking()">Change value</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  data () {
+    return {
+      price: 200
+    }
+  },
+  methods: {
+    clicking () {
+      this.price = this.price + 10
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-cli/src/components/FormUser.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+      <div class="panel panel-primary">
+        <div class="panel-heading">
+          <h3 class="panel-title">Add User</h3>
+        </div>
+        <div class="panel-body">
+          <form>
+            <div class="form-group">
+              <label for="name">Name</label>
+              <input type="text" id="name" class="form-control" placeholder="Name" v-model="name">
+            </div>
+            <div class="form-group">
+              <label for="name">Email</label>
+              <input type="text" id="email" class="form-control" placeholder="Email" v-model="email">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+      <div class="panel panel-success">
+        <div class="panel-body">
+          My Name is: {{ name }}
+          <br/>
+          My Email is: {{ email }}
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'FormUser',
+  data () {
+    return {
+      name: '',
+      email: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
